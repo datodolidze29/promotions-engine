acc74c8542728088dc61459ce6505411ae0dddb4
datodolidze29 <142981700+datodolidze29@users.noreply.github.com>
2026-09-01 18:40:53 +0400

    ORMIMPLEMENTATION

--- src/Service/Serializer/DTOSerializer.php
+++ src/Service/Serializer/DTOSerializer.php
@@ -3,10 +3,12 @@
 namespace App\Service\Serializer;
 
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
 use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Mapping\Loader\AttributeLoader;
 
 class DTOSerializer implements SerializerInterface
 {
@@ -15,7 +17,12 @@ class DTOSerializer implements SerializerInterface
     public function __construct()
     {
         $this->serializer = new Serializer(
-            [new ObjectNormalizer(nameConverter: new CamelCaseToSnakeCaseNameConverter())],
+            [
+                new ObjectNormalizer(
+                    classMetadataFactory: new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader())),
+                    nameConverter: new CamelCaseToSnakeCaseNameConverter(),
+                ),
+            ],
             [new JsonEncoder()],
         );
     }
